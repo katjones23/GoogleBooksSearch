a5aa776e980df2a5e6d14a51d021b6abc1ad002b
Katherine Jones <kejone1414@gmail.com>
2020-08-01 13:33:17 -0400

    feat: added api routes for books db

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,6 +1,22 @@
 const router = require("express").Router();
 const db = require("../models");
 
-// add code for router
+router.get("/books", (req, res) => {
+    db.Book.find({})
+    .then(books => res.json(books))
+    .catch(err => res.status(422).json(err));
+});
+
+router.post("/books", (req, res) => {
+    db.Book.create(req.body)
+    .then(book => res.json(book))
+    .catch(err => res.status(400).json(err));
+});
+
+router.delete("/books/:id", (req, res) => {
+    db.Book.deleteOne({ _id: req.params.id })
+    .then(book => res.json(book))
+    .catch(err => res.status(400).json(err));
+});
 
 modeul.exports = router;
