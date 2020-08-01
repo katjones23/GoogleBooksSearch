bdf77a43d3498d541eedc2e038f42119157cdeb1
Katherine Jones <kejone1414@gmail.com>
2020-08-01 12:20:22 -0400

    feat: added foundation of routes.js

---
+++ routes/apiRoutes.js
@@ -0,0 +1,6 @@
+const router = require("express").Router();
+const db = require("../models");
+
+// add code for router
+
+modeul.exports = router;
--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const path = require("path");
+const mongoose = require("mongoose");
 const PORT = process.env.PORT || 3001;
 const app = express();
+const apiRoutes = require("./routes/apiRoutes")
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -10,7 +12,13 @@ if (process.env.NODE_ENV === "production") {
     app.use(express.static("client/build"));
 }
 
+mongoose.connect(
+    process.env.MONGODB_URI || "mongodb://localhost/googlebooksreact",
+    { useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true }
+);
+
 // API routes go here
+app.use("/api", apiRoutes)
 
 app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "./client/build/index.html"));
