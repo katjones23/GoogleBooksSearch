c7e1199cf0b47ce77f5ac2800f9a042e72e125ba
Katherine Jones <kejone1414@gmail.com>
2020-08-01 15:18:50 -0400

    feat: added foundation of results components

---
+++ client/src/componenets/Results/index.js
@@ -0,0 +1,40 @@
+import React from "react";
+
+export function Results({ children }) {
+    return <div className="container">{children}</div>;
+}
+
+export function ResultsItem({ title, authors, image, description }) {
+    return (
+        <div className="container">
+
+            <div className="row">
+                <div className="col-4">
+                    <h3>{title}</h3>
+                </div>
+                <div className="col-4 offset-4">
+                    {/* view button */}
+                    {/* save button */}
+                </div>
+            </div>
+
+            <div className="row">
+                <div className="col-4">
+                    <h5>Written By {authors}</h5>
+                </div>
+            </div>
+
+            <div className="row">
+                <div className="col-3">
+                    {/* image */}
+                </div>
+                <div className="col-9">
+                    <p>{description}</p>
+                </div>
+            </div>
+
+        </div>
+    );
+}
+
+// title, authors, description, image, link
